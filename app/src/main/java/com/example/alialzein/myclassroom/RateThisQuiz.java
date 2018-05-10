df0929a739fea8dd0218584d084ff6bf7afa0d45
ALiAlzein <alizein1997@gmail.com>
2018-05-10 22:31:50 +0300

    10/5 at 10:30 pm

--- app/src/main/java/com/example/alialzein/myclassroom/RateThisQuiz.java
+++ app/src/main/java/com/example/alialzein/myclassroom/RateThisQuiz.java
@@ -51,6 +51,7 @@ public class RateThisQuiz extends AppCompatActivity {
                         }
                     }
                 });
+                finish();
             }
         });
 
